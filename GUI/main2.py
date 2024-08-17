099facca14d4b90daed9241168074be554e2269f
DarkOvs77 <75710806+DarkOverlord689@users.noreply.github.com>
2024-08-16 23:15:19 -0500

    Update main2.py
    Comentarios

--- GUI/main2.py
+++ GUI/main2.py
@@ -1,9 +1,21 @@
+
 import sys
 from PyQt6.QtWidgets import QApplication
 from melanoma_detector import MelanomaDetector
 
 if __name__ == '__main__':
+    """
+    Este script es la función principal y crea una aplicación PyQt para mostrar la interfacce gráfica
+    """
+
+    # Inicia la aplicación PyQt
     app = QApplication(sys.argv)
+
+    # Crea una instancia de MelanomaDetector el cual es la función donde están las funcionalidades
     ex = MelanomaDetector()
+
+    # Muestra la ventana
     ex.show()
+
+    # Sale  de la aplicación cuando se cierre la ventan
     sys.exit(app.exec())
