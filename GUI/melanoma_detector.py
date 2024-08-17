d9e9e4abddb9763e2d34317bc647efa344ef29fe
DarkOvs77 <75710806+DarkOverlord689@users.noreply.github.com>
2024-08-17 00:08:45 -0500

    Update melanoma_detector.py
    Comentarios

--- GUI/melanoma_detector.py
+++ GUI/melanoma_detector.py
@@ -1,19 +1,31 @@
+"""
+    Este mpodulo posee las funciones pricipales que se ejecutan en las interfaz gráfica
+    Este hace una importación del módulo de componentes
+    Esta diseñado con una clase principal y funciones 
+
+"""
+
+# ------------------------ IMportación de librerias--------------------------------
 import os
 import csv
 from datetime import datetime
-from PyQt6.QtWidgets import QMainWindow, QWidget, QHBoxLayout, QFileDialog, QSplitter, QComboBox, QLabel, QVBoxLayout, QPushButton
+from PyQt6.QtWidgets import QMainWindow, QWidget, QHBoxLayout, QFileDialog,  QLabel
 from PyQt6.QtGui import QPixmap, QFont
 from PyQt6.QtCore import Qt
 from load_and_predict import  predicto, ImagePreprocessor
-from image_viewer import ImageViewer
 from ui_components import create_left_layout, create_right_widget
 from PyQt6.QtWidgets import QTableWidgetItem
 
 
 
+# Clase principal MelanomaDetector que hereda de QMainWindow para gestionar la ventana principal de la GUI.
+
+
 class MelanomaDetector(QMainWindow):
     def __init__(self):
+         # Constructor de la clase, inicializa variables y configura la interfaz
         super().__init__()
+        # se crea un diccionario que mapea nombres de modelos a los archivos correspondientes
         self.models = {
             'DenseNet': 'best_model_DenseNet121.h5',
             'ResNet': 'best_model_ResNet50.h5',
@@ -22,10 +34,13 @@ class MelanomaDetector(QMainWindow):
             'Inception': 'best_model_InceptionV3.h5',
             'EfficientNet': 'best_model_EfficientNetV2B0.h5'
         }
+
+        # Variables para el modelo actual, preprocesador de imágenes e historial de imágenes
         self.current_model = None
         self.preprocessor = ImagePreprocessor()
         self.image_history = []
         self.current_image = None
+        # Diccionario para traducir abreviaciones de tipos de cáncer a nombres completos
         self.cancer_types = {
             'mel': 'Melanoma',
             'nv': 'Nevo',
@@ -34,33 +49,44 @@ class MelanomaDetector(QMainWindow):
             'bkl': 'Queratosis Benigna',
             'df': 'Dermatofibroma',
         }
+        # Inicializa la interfaz gráfica
         self.initUI()
 
+
+    # Función para actualizar el nivel de zoom en la imagen
     def update_zoom(self, value):
-        if hasattr(self, 'image_viewer'):
+        if hasattr(self, 'image_viewer'):   
             self.image_viewer.update_zoom(value)
 
+    # Función para actualizar el contraste en la imagen
     def update_contrast(self, value):
         if hasattr(self, 'image_viewer'):
             self.image_viewer.update_contrast(value)
 
+    # Función para actualizar el brillo en la imagen
     def update_brightness(self, value):
         if hasattr(self, 'image_viewer'):
             self.image_viewer.update_brightness(value)
 
-
+    # Configuración inicial de la interfaz gráfica de usuario (UI)
     def initUI(self):
-        self.setWindowTitle('Detector de Melanoma Avanzado')
+
+        # Configura el título de la ventana y su tamaño
+        self.setWindowTitle('Dermadetect')
         self.setGeometry(100, 100, 1200, 800)
 
+
+        # Crea el widget central y el layout principal
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
-
         main_layout = QHBoxLayout(central_widget)
 
-        left_layout = create_left_layout(self)
-        right_widget = create_right_widget(self)
+        # Crea los layouts izquierdo y derecho para la interfaz utilizando funciones personalizadas
 
+        left_layout = create_left_layout(self) #componente izquierdo
+        right_widget = create_right_widget(self) #componente derecho
+
+        # Agrega ambos layouts al layout principa
         main_layout.addLayout(left_layout)
         main_layout.addWidget(right_widget)
 
@@ -74,6 +100,8 @@ class MelanomaDetector(QMainWindow):
         model_path = self.models[selected_model]
         self.current_model = load_trained_model(model_path)"""
 
+
+    # Función para cargar una imagen desde el sistema de archivos
     def load_image(self):
         file_name, _ = QFileDialog.getOpenFileName(self, "Seleccionar Imagen", "", "Archivos de Imagen (*.png *.jpg *.bmp)")
         if file_name:
@@ -96,14 +124,16 @@ class MelanomaDetector(QMainWindow):
         self.history_table.setItem(row_position, 1, QTableWidgetItem(predicted_class))
         self.history_table.setItem(row_position, 2, QTableWidgetItem(f"{probability:.2f}%"))
     """
+     # Función para cargar una imagen desde el historial
     def load_from_history(self, item):
-        row = item.row()
-        file_name = self.history_table.item(row, 0).text()
-        pixmap = QPixmap(self.image_history[row])
-        self.image_viewer.set_image(pixmap)
-        self.current_image = file_name
+        row = item.row()  # Obtiene la fila seleccionada
+        file_name = self.history_table.item(row, 0).text()  # Obtiene el nombre de la imagen de la tabla
+        pixmap = QPixmap(self.image_history[row])  # Carga la imagen del historial
+        self.image_viewer.set_image(pixmap)  # La muestra en el visor
+        self.current_image = file_name  # Actualiza la imagen actual
 
 
+    # Actualiza la pestaña de comparación con imágenes y descripciones
     def update_comparison_tab(self, images_and_descriptions):
         # Limpiar el layout actual
         while self.comparison_layout.count():
@@ -132,11 +162,16 @@ class MelanomaDetector(QMainWindow):
             description_label.setWordWrap(True)
             self.comparison_layout.addWidget(description_label, row * 2 + 1, col, alignment=Qt.AlignmentFlag.AlignCenter)
 
+    """
+    Esta es la funcion mas importante, aqui se realiza la predicción del modelo
+    analiza la imagen actual y muestra los resultados
+    """
     def analyze_image(self):
         if not self.current_image:
             self.result_text.setText("<p style='color: red;'>Por favor, cargue una imagen primero.</p>")
             return
 
+        # Recolecta los datos del paciente
         patient_data = {
             "Nombre": self.name_input.text(),
             "Identificación": self.id_input.text(),
@@ -144,11 +179,12 @@ class MelanomaDetector(QMainWindow):
             "Sexo": self.sex_input.currentText(),
             "Localización": self.location_input.currentText()
         }
-
+        # Llama a la función de predicción del modelo seleccionado
         result = predicto(self.current_model, self.current_image, patient_data["Edad"], patient_data["Sexo"], patient_data["Localización"])
 
         # Convertir abreviaciones a nombres completos
         full_class_name = self.cancer_types.get(result['predicted_class'], result['predicted_class'])
+         # Muestra los resultados predichos y las probabilidadesx|
         result_text = f"""
         <h3>Resultados del Análisis</h3>
         <p><b>Nombre:</b> {patient_data['Nombre']} <br>
@@ -160,11 +196,16 @@ class MelanomaDetector(QMainWindow):
         <h4>Probabilidades:</h4>
         <ul>
         """
+
+         # Lista las probabilidades para cada clase
         for class_name, probability in result['probabilities'].items():
             full_name = self.cancer_types.get(class_name, class_name)
             result_text += f"<li><b>{full_name}:</b> {probability:.4f}</li>"
 
         result_text += "</ul>"
+
+
+        # Recomendación según la probabilidad más alta
         max_probability = max(result['probabilities'].values())
 
         if max_probability > 0.5:
@@ -174,9 +215,14 @@ class MelanomaDetector(QMainWindow):
 
         result_text += recommendation
 
+        # Actualiza la pestaña de resultados con el texto generado
         self.result_text.setHtml(result_text)
+
+        # Guarda los resultados en un archivo CSV
         self.save_to_csv(patient_data, max_probability, full_class_name)
 
+        
+        # Actualiza la pestaña de comparación con imágenes de diferentes modelos
         images_and_descriptions = [
             ('interpretation_DenseNet121.jpg', 'Modelo DenseNet21'),
             ('interpretation_EfficientNetV2B0.jpg', 'Modelo EfficientNet'),
@@ -190,7 +236,7 @@ class MelanomaDetector(QMainWindow):
         self.update_comparison_tab(images_and_descriptions)
 
 
-
+    # Función para guardar los resultados en un archivo CSV
     def save_to_csv(self, patient_data, probabilities, predicted_class):
         csv_file = 'historial_pacientes.csv'
         file_exists = os.path.isfile(csv_file)
@@ -216,6 +262,7 @@ class MelanomaDetector(QMainWindow):
 
         self.update_patient_history_table()
 
+    # Función que actuaiza la pestaña pasicentes
     def update_patient_history_table(self):
         self.history_table.setRowCount(0)  # Limpiar la tabla
 
@@ -248,6 +295,7 @@ class MelanomaDetector(QMainWindow):
 
             self.result_text.setText(history)
 
+    #guarda los resultados en un txt
     def save_results(self):
         if not self.current_image:
             return
