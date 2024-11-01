99a255a6e04e6398e961a5883f17a9b33e828a58
DarkOvs77 <75710806+DarkOverlord689@users.noreply.github.com>
2024-10-31 22:53:52 -0500

    Update main.py
    Configuración del CORS

--- api/main.py
+++ api/main.py
@@ -6,7 +6,19 @@ from app.db.crud import get_db
 
 
 app = FastAPI()
+# Configuración de CORS
+origins = [
+    "http://localhost:3000",  
+    "http://127.0.0.1:3000",
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # Crear tablas en la base de datos
 Base.metadata.create_all(bind=engine)
 
